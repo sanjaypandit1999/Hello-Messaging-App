4a3685e2d398a5514eb47418cee10876ced91dab
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-05 01:01:08 +0530

    Welcome to messaging app

---
+++ HelloMessagingApp/src/test/java/com/bridgelabz/message/HelloMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
