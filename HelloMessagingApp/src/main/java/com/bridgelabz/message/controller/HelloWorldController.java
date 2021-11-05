a18a7819cdc59c91edc869fa1bf3a67eb66aa797
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-05 12:33:34 +0530

    ADDED: Use GET request method  and pass name as a query parameter

--- HelloMessagingApp/src/main/java/com/bridgelabz/message/controller/HelloWorldController.java
+++ HelloMessagingApp/src/main/java/com/bridgelabz/message/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.message.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class HelloWorldController {
 	@RequestMapping(value={"","/","hello"})
 	public String sayHello() {
 		return "Hello I am sanjay";
+	}
+	
+	@RequestMapping(value = {"/quer"}, method = RequestMethod.GET)
+	public String sayHelloParam(@RequestParam(value = "firstName") String fName, @RequestParam(value = "lastName") String lName) {
+		return "Hello" +fName +""+lName;
 	}
 }
 
